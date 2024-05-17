5e88f94c62301e721567f828ae4866ea4f851d62
Zedcsuper <161995013+Zedcsuper@users.noreply.github.com>
2024-05-17 19:33:03 +0300

    Add files via upload

---
+++ ft_util.c
@@ -0,0 +1,59 @@
+#include "ft.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+char	*ft_strdup(char *src)
+{
+	unsigned long long int		i;
+	unsigned long long int		len;
+	char		*dst;
+
+	len = 0;
+	while (src[len] != '\0')
+		len++;
+	if (!(dst = (char *)malloc(sizeof(char) * (len + 1))))
+		return (0);
+	i = 0;
+	while (i < len)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
+
+unsigned long long int	ft_atoi(const char *str)
+{
+	unsigned long long int	res;
+	int	negative;
+
+	negative = 1;
+	res = 0;
+	while (*str && (*str == ' ' || *str == '\n' || *str == '\t' ||
+			*str == '\v' || *str == '\f' || *str == '\r'))
+		++str;
+	if (*str == '-')
+		negative = -1;
+	if (*str == '-' || *str == '+')
+		++str;
+	while (*str && *str >= '0' && *str <= '9')
+	{
+		res = res * 10 + (*str - 48);
+		++str;
+	}
+	return (res * negative);
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
